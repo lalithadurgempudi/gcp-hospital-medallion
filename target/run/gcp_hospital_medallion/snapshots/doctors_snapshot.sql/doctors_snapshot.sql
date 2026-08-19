efc920feb5617d8f2b6663df99af17a8bee63f71
teamvenkat <venkatdurgempudi@gmail.com>
2026-08-19 13:44:32 +0530

    materialized field correction : dbt build

--- target/run/gcp_hospital_medallion/snapshots/doctors_snapshot.sql/doctors_snapshot.sql
+++ target/run/gcp_hospital_medallion/snapshots/doctors_snapshot.sql/doctors_snapshot.sql
@@ -1,6 +1,6 @@
 
       merge into `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`doctors_snapshot` as DBT_INTERNAL_DEST
-    using `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`doctors_snapshot__dbt_tmp203038062040` as DBT_INTERNAL_SOURCE
+    using `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`doctors_snapshot__dbt_tmp134249442768` as DBT_INTERNAL_SOURCE
     on DBT_INTERNAL_SOURCE.dbt_scd_id = DBT_INTERNAL_DEST.dbt_scd_id
 
     when matched
