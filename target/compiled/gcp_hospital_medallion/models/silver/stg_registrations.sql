50b5f5524bfbdb12abf7ea82ec47921046556c8b
teamvenkat <venkatdurgempudi@gmail.com>
2026-08-19 12:25:00 +0530

    dbt changes

--- target/compiled/gcp_hospital_medallion/models/silver/stg_registrations.sql
+++ target/compiled/gcp_hospital_medallion/models/silver/stg_registrations.sql
@@ -3,3 +3,13 @@
 SELECT *
 FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_bronze`.`registrations`
 
+
+
+WHERE updated_at >= TIMESTAMP_SUB(
+    (
+        SELECT MAX(updated_at)
+        FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_registrations`
+    ),
+    INTERVAL 1 HOUR
+)
+
--- target/run/gcp_hospital_medallion/models/silver/stg_registrations.sql
+++ target/run/gcp_hospital_medallion/models/silver/stg_registrations.sql
@@ -1,20 +1,45 @@
-
+-- back compat for old kwarg name
   
+  
+        
+            
+            
+            
+            
+        
     
 
-    create or replace table `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_registrations`
-      
-    
     
 
-    
-    OPTIONS()
-    as (
-      
+    merge into `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_registrations` as DBT_INTERNAL_DEST
+        using (
 
 SELECT *
 FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_bronze`.`registrations`
 
 
-    );
-  
+
+WHERE updated_at >= TIMESTAMP_SUB(
+    (
+        SELECT MAX(updated_at)
+        FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_registrations`
+    ),
+    INTERVAL 1 HOUR
+)
+
+
+        ) as DBT_INTERNAL_SOURCE
+        on ((DBT_INTERNAL_SOURCE.registration_id = DBT_INTERNAL_DEST.registration_id))
+
+    
+    when matched then update set
+        `registration_id` = DBT_INTERNAL_SOURCE.`registration_id`,`first_name` = DBT_INTERNAL_SOURCE.`first_name`,`last_name` = DBT_INTERNAL_SOURCE.`last_name`,`date_of_birth` = DBT_INTERNAL_SOURCE.`date_of_birth`,`gender` = DBT_INTERNAL_SOURCE.`gender`,`city` = DBT_INTERNAL_SOURCE.`city`,`registration_date` = DBT_INTERNAL_SOURCE.`registration_date`,`status` = DBT_INTERNAL_SOURCE.`status`,`created_at` = DBT_INTERNAL_SOURCE.`created_at`,`updated_at` = DBT_INTERNAL_SOURCE.`updated_at`
+    
+
+    when not matched then insert
+        (`registration_id`, `first_name`, `last_name`, `date_of_birth`, `gender`, `city`, `registration_date`, `status`, `created_at`, `updated_at`)
+    values
+        (`registration_id`, `first_name`, `last_name`, `date_of_birth`, `gender`, `city`, `registration_date`, `status`, `created_at`, `updated_at`)
+
+
+    
